23992584bfcce40cbe08564000022c4b5fe6cc36
Bar <ibari02@gmail.com>
2020-10-19 17:17:03 +0300

    Initial Commit

---
+++ ex0_OS/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ex0_OS
+//
+//  Created by Bar Ifrah on 19/10/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
