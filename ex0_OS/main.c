08824250fcaa80f66c470b8d1056faf48447fe64
sharonlevi94 <73107447+sharonlevi94@users.noreply.github.com>
2020-10-21 07:58:29 +0300

    Update main.c
    סידרתי קצת את הקוד,
    הוספתי את הפקודה לסוף הקובץ
    הרצתי וזה עובד

--- ex0_OS/main.c
+++ ex0_OS/main.c
@@ -35,49 +35,52 @@ int main(){
     fscanf(file_one, "%s", stud_name1);
     fscanf(file_two, "%s", stud_name2);
 //------------------------------------------------------------------
-    //int *grade = NULL;
-    // runs untill one file is NULL(EOF)
-    while (file_one != NULL && file_two != NULL){
+    // runs until one file is NULL(EOF)
+    while (!feof(file_one) && !feof(file_two))
+    	{
         // compares names between two files. if 1st name comes first in alphabetical order- goes into 'if'.
-        if (strcmp(stud_name1, stud_name2) < 0){
-            //we have to send the name to the output file - in this casem the name from the first file.
-//            fprintf(output, "%s",stud_name1);               // prints the first student name to output file.
-//            printf("%s ", stud_name1);                      // prints the first student name to console.
-//            print_grades_to_output(file_one, output);       // prints the rest of the line.
-//            fscanf(file_one, "%s", stud_name1);             // reads the next student name in the file.
+         if (strcmp(stud_name1, stud_name2) < 0)
+         {
+            //we have to send the name to the output file - in this case the name from the first file.
             print_student_name(file_one, output, stud_name1);
-            
-        } else if (strcmp(stud_name1, stud_name2) > 0){     // if the second name is first in alphabetical order- goes into 'else if'.
-//            fprintf(output, "%s",stud_name2);               // prints the second student name to output file.
-//            printf("%s ", stud_name2);                      // prints the second student name to console.
-//            print_grades_to_output(file_two, output);       // prints the rest of the line.
-//            fscanf(file_two, "%s", stud_name2);             // reads the next student name in the file.
-            print_student_name(file_two, output, stud_name2);
-        } else{                                             // if both names are the same- will go in 'else'
-            print_student_name(file_one, output, stud_name1);
-            print_grades_to_output(file_two, output);       // to make one line with the student name.
-            fscanf(file_two, "%s", stud_name2);             // for both files- because they held the same name.
-        }
+
+         }
+         else
+        	 if (strcmp(stud_name1, stud_name2) > 0)
+             {     // if the second name is first in alphabetical order- goes into 'else if'.
+                print_student_name(file_two, output, stud_name2);
+             }
+        	 else
+        	 {  // if both names are the same- will go in 'else'
+                print_student_name(file_one, output, stud_name1);
+                print_grades_to_output(file_two, output);   // to make one line with the student name.
+                fscanf(file_two, "%s", stud_name2);         // for both files- because they held the same name.
+             }
     }
-    while (file_one != NULL) {
+
+    while (!feof(file_one))
+    {
         print_student_name(file_one, output, stud_name1);
     }
-    while (file_two != NULL) {
+    while (!feof(file_two))
+    {
         print_student_name(file_two, output, stud_name2);
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 //------------------------------------------------------------------
     int terminate(FILE *p2file){
-    if (p2file == NULL) {
+    if (p2file == NULL)
+    {
         printf("can't open file\n");
         exit(1);
     }
     return 0;
 }
 //------------------------------------------------------------------
-FILE *open_files(char *opening_mode){
+FILE *open_files(char *opening_mode)
+{
     char *file_name = NULL;
     unsigned long file_len;
     size_t filename_len = MAX_FILE_NAME_LEN;  // as size_t to use in 'getline'
@@ -90,19 +93,22 @@ FILE *open_files(char *opening_mode){
     return file_to_open;
 }
 //------------------------------------------------------------------
-void print_grades_to_output(FILE *input_file, FILE *output_file) {
+void print_grades_to_output(FILE *input_file, FILE *output_file)
+{
     char rest_of_line_grades;
     fscanf(input_file, "%c", &rest_of_line_grades);        // inserts a grade to 'grade'.
-    while (rest_of_line_grades != '\n') {
+    while (rest_of_line_grades != '\n')
+    {
         fprintf(output_file, "%c", rest_of_line_grades);    // prints 'grade' into output file.
-        printf("%c", rest_of_line_grades);                  // prints 'grade' to consule.
+        printf("%c", rest_of_line_grades);                  // prints 'grade' to console.
         fscanf(input_file, "%c", &rest_of_line_grades);        // inserts a grade to 'grade'.
     }
     fprintf(output_file, "%c", '\n');
     printf("\n");
 }
 //------------------------------------------------------------------
-void print_student_name(FILE *input_file, FILE *output_file, char *stud_name) {
+void print_student_name(FILE *input_file, FILE *output_file, char *stud_name)
+{
     fprintf(output_file, "%s", stud_name);               // prints the first student name to output file.
     printf("%s ", stud_name);                      // prints the first student name to console.
     print_grades_to_output(input_file, output_file);       // prints the rest of the line.
